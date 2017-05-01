235216e91641c319c43ec0ad00b6456ead0c678e
mkfreeman <michael.k.freeman@gmail.com>
2017-05-01 10:06:06 -0400

    Replaced exercises with demos

---
+++ demo-4/README.md
@@ -0,0 +1,8 @@
+## Exercise 2: Building a Scatterplot
+
+This exercise is a more robust opportunity to visualize data using D3. Data on the life expectancy and GDP of countries has been prepared after downloading it from the [World Bank Website](http://data.worldbank.org/data-catalog/world-development-indicators). As usual, once you fork and clone this repository, instructions will appear in the `js/main.js` files.
+
+Your final product should look like this
+
+![example 1 complete](imgs/complete.png)
+
---
+++ demo-4/js/main.js
@@ -0,0 +1,65 @@
+/* Create a scatter plot of 1960 life expectancy (gdp) versus 2013 life expectancy (life_expectancy).*/
+
+$(function() {
+    // Variables to show
+    var xVar = 'gdp';
+    var yVar = 'life_expectancy';
+    var chartData,
+        nestedData;
+
+    // Load data in using d3's csv function.
+    d3.csv('data/prepped_data.csv', function(error, data) {
+        // Put data into generic terms
+        var prepData = function() {
+            chartData = data.map(function(d) {
+                return {
+                    x: d[xVar],
+                    y: d[yVar],
+                    id: d.country,
+                    region: d.region
+                };
+            });
+
+            // Nest data by region
+            nestedData = d3.nest()
+                .key(function(d) {
+                    return d.region;
+                })
+                .entries(chartData);
+        };
+
+        prepData();
+        // Define function to draw ScatterPlot
+        var scatter = ScatterPlot().width(300).height(300);
+
+        // Function to make charts (doing a data-join to make charts)
+        var draw = function() {
+            // Prep data
+            prepData();
+
+            // Do a data join to make small multiples
+            var charts = d3.select('#vis').selectAll('.chart')
+                .data(nestedData)
+
+            charts.enter().append("div")
+                .attr('class', 'chart')
+                .merge(charts)
+                .call(scatter);
+
+            charts.exit().remove();
+        };
+
+        // Call draw function
+        draw();
+
+        // Set change event to the select menu
+        $('select').on('change', function(d) {
+            xVar = $(this).val();
+            draw();
+        });
+
+        // Initialize materialize style
+        $('select').material_select()
+
+    });
+});
